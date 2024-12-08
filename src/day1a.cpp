1edd828b4f9d2515ae205b1112f585fae0015cb0
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-12-08 15:07:49 -0600

    Add find_smallest()

--- src/day1a.cpp
+++ src/day1a.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 #include <memory>
-const std::string DELIM = "   ";
+#include <limits>
 
 std::vector<std::string> readlines(std::string filepath) {
     std::string line;
@@ -15,11 +15,23 @@ std::vector<std::string> readlines(std::string filepath) {
         }
     }
     input.close();
-    std::cout << lines.size() << std::endl;
     return lines;
 }
 
+int find_smallest(const std::vector<int>& list) {
+    int smallest = list.front();
+    for (int i=1; i < list.size(); i++) {
+        if (list.at(i) < smallest) {
+            smallest = list.at(i);
+            std::cout << smallest << std::endl;
+        }
+    }
+    return smallest;
+}
+
 int main() {
+    const std::string DELIM = "   "; // For parsing the text file lines
+    const int INT_MAX = std::numeric_limits<int>::max(); // Used for sillyness later :)
     auto lines = readlines("build/input/day1.txt");
     std::vector<int> col1, col2;
 
@@ -32,5 +44,6 @@ int main() {
     }
 
 
+    std::cout << find_smallest(col1) << std::endl;
     return 0;
 }
