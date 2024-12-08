0f2536af0f2e1c26ce4f9f40e016a4b79b962157
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-12-08 14:54:47 -0600

    day1a string parsing

--- Makefile
+++ Makefile
@@ -1,5 +1,17 @@
-day1a: build_folder
-	g++ src/day1a.cpp -o build/day1a
+TARGETS := day1a
+CXX = g++
+CXXFLAGS = -Wall -g
+obj_filfes = helper.o
+day1a: input
+	g++ ${CXXFLAGS} src/day1a.cpp -o build/$@
+
+input:
+	cp -r src/input build/input
 
 build_folder:
 	mkdir build
+
+all: ${TARGETS}
+
+run: day1a
+	./build/day1a
--- src/day1a.cpp
+++ src/day1a.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <memory>
+const std::string DELIM = "   ";
+
+std::vector<std::string> readlines(std::string filepath) {
+    std::string line;
+    std::ifstream input(filepath);
+    std::vector<std::string> lines;
+    if (input.is_open()) {
+        while (getline(input, line)) {
+            lines.push_back(line);
+        }
+    }
+    input.close();
+    std::cout << lines.size() << std::endl;
+    return lines;
+}
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    auto lines = readlines("build/input/day1.txt");
+    std::vector<int> col1, col2;
+
+    for (int i=0; i<lines.size(); i++) {
+        std::string line = lines.at(i);
+        auto col1num = line.substr(0, line.find(DELIM));
+        auto col2num = line.substr(line.find(DELIM), line.size());
+        col1.push_back(stoi(col1num));
+        col2.push_back(stoi(col2num));
+    }
+
 
     return 0;
 }
