eba48d880d0e3bcc8c59d88a87942b38f6249481
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-12-11 14:36:29 -0600

    Solve day3a

--- src/day3a.cpp
+++ src/day3a.cpp
@@ -4,20 +4,29 @@
 #include "helper.hpp"
 
 int main() {
-    std::vector<std::string> lines = readlines("input/day3_test.txt");
+    std::vector<std::string> lines = readlines("input/day3.txt");
     std::regex mul_regex("mul\\(\\d{1,3},\\d{1,3}\\)");
     std::cout << lines.size() << std::endl;
+
+    int sum = 0;
     for (int i=0; i < lines.size(); i++) {
         auto line = lines[i];
-        std::cout << line << std::endl;
         auto op_begin = std::sregex_iterator(line.begin(), line.end(), mul_regex);
         auto op_end = std::sregex_iterator();
 
-        std::cout << "Found " << std::distance(op_begin, op_end) << " ops.\n";
         for (std::regex_iterator i = op_begin; i != op_end; i++) {
-            
+            int product = 1;
+            auto match = i->str();
+            std::regex num("\\d{1,3}");
+            auto mul_begin = std::sregex_iterator(match.begin(), match.end(), num);
+            auto mul_end = std::sregex_iterator();
+            for (std::regex_iterator i = mul_begin; i != mul_end; i++) {
+                product *= stoi(i->str());
+            }
+            sum += product;
         }
     }
 
+    std::cout << sum << std::endl;
     return 0;
 }
